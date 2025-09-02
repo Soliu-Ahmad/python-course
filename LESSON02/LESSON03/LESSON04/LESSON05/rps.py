52741222a9aedb527c0cfd75b3c7406774c016b5
MAC <mac@MACs-MacBook-Pro.local>
2025-09-02 23:15:59 +0100

    continuation of the project

--- LESSON02/LESSON03/LESSON04/LESSON05/rps.py
+++ LESSON02/LESSON03/LESSON04/LESSON05/rps.py
@@ -1,5 +1,17 @@
 import sys
 import random
+from enum import Enum
+
+class Choice(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+    
+# print(Choice(2))
+# print(Choice.ROCK)
+# print(Choice["ROCK"])
+# print(Choice.ROCK.value)
+# sys.exit()
 
 print('')
 playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n")
@@ -9,12 +21,23 @@ if player < 1 | player > 3:
     sys.exit("you must enter 1, 2 or 3.")
     
 
-computerchoice = random.choice("123")
+computerchoice = random.choice("123")   
 
 compuetr = int(computerchoice)
 
 print("")
-print("you chose " + playerchoice + ".")
-print("python chose " + computerchoice + ".")
+print("you chose " + str(Choice(player)).replace("Choice.", "") + ".")
+print("python chose " + str(Choice(compuetr)).replace("Choice.", "") + ".")
 
 print("")
+
+if player == 1 and compuetr == 3:
+    print("you win!")
+elif player == 2 and compuetr == 1:
+    print("you win!")
+elif player == 3 and compuetr == 2:
+    print("you win!")
+elif player == compuetr:
+    print("it's a tie!")
+else:    
+    print("you win!")
