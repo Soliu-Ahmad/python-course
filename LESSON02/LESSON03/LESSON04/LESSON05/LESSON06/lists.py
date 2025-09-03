d158f32c349296075d02ac7aa807ba655c686c29
MAC <mac@MACs-MacBook-Pro.local>
2025-09-04 00:07:40 +0100

    Lists

---
+++ LESSON02/LESSON03/LESSON04/LESSON05/LESSON06/lists.py
@@ -0,0 +1,4 @@
+users = ['David', 'Alice', 'Bob', 'Cathy']
+data = ['David', 20, 30, 40, True]
+empty = []
+print('David' in users)
--- LESSON02/LESSON03/LESSON04/LESSON05/rps.py
+++ LESSON02/LESSON03/LESSON04/LESSON05/rps.py
@@ -17,7 +17,7 @@ print('')
 playerchoice = input("Enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n")
 
 player = int(playerchoice)
-if player < 1 | player > 3:
+if player < 1 or player > 3:
     sys.exit("you must enter 1, 2 or 3.")
     
 
